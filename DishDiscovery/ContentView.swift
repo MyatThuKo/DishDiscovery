a76ea1cdb0046b1cc4ac90dea836b2a8a04b86b0
Myat Thu Ko (Tony) <nymyatthuko@gmail.com>
2024-10-10 09:02:37 -0400

    Initial Commit

---
+++ DishDiscovery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DishDiscovery
+//
+//  Created by Myat Thu Ko on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DishDiscovery/DishDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishDiscoveryApp.swift
+//  DishDiscovery
+//
+//  Created by Myat Thu Ko on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DishDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
